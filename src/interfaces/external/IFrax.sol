f1af7a53d9bff1417142b664c9fdd96cbbf538d3
Ferdi Kurt <ferdi.kurt111@gmail.com>
2025-09-10 14:11:08 +0300

    external-IFrax interface

---
+++ src/interfaces/external/IFrax.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+/// @dev Minimal stubs; expanded in Week 8.
+interface IFraxAMO {
+    function deposit(uint256 amount) external;
+    function withdraw(uint256 amount) external;
+}
