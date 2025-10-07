342d0e2cefd60ca06b0b908dc1519e2fdd1c3aed
Ferdi Kurt <ferdi.kurt111@gmail.com>
2025-10-07 17:23:22 +0300

    VaultCore

---
+++ src/core/VaultCore.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+import {VaultStorage} from "./VaultStorage.sol";
+import {Roles} from "./Roles.sol";
+
+
