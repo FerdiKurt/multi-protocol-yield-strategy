a5e7b0824993807a306147d44d8565ef753e7ad9
Ferdi Kurt <ferdi.kurt111@gmail.com>
2025-10-08 15:03:25 +0300

    ignore this commit

--- test/OracleRouter.t.sol
+++ test/OracleRouter.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.28;
 
 import "forge-std/Test.sol";
 import {OracleRouter} from "../src/oracle/OracleRouter.sol";
@@ -25,8 +25,9 @@ contract OracleRouterTest is Test {
     }
 
     function testGetPriceScalesTo1e18() public view {
-        (uint256 value, uint256 ts) = (router.getPrice(address(0xAAA), address(0xBBB)).value,
-                                       router.getPrice(address(0xAAA), address(0xBBB)).lastUpdate);
+        (uint256 value, uint256 ts) =
+            (router.getPrice(address(0xAAA), address(0xBBB)).value,
+             router.getPrice(address(0xAAA), address(0xBBB)).lastUpdate);
         assertEq(value, 100e18); // scaled to 1e18
         assertTrue(ts != 0);
     }
