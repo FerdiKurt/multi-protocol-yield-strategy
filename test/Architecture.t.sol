223fda26d631b8e98901832cac777c1f44dfed88
Ferdi Kurt <ferdi.kurt111@gmail.com>
2025-10-08 15:03:12 +0300

    test refactor

--- test/Architecture.t.sol
+++ test/Architecture.t.sol
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.28;
 
 import "forge-std/Test.sol";
 import {VaultStorage} from "../src/core/VaultStorage.sol";
+import {Roles} from "../src/core/Roles.sol";
 
 contract VaultStorageHarness is VaultStorage {
-    constructor(address admin, address asset_, address treasury_) VaultStorage(admin, asset_, treasury_) {}
-    function pause() external onlyRole(keccak256("ROLE_ADMIN")) { _pause(); }
-    function unpause() external onlyRole(keccak256("ROLE_ADMIN")) { _unpause(); }
+    constructor(address admin, address treasury_) VaultStorage(admin, treasury_) {}
+    function pause() external onlyRole(Roles.ROLE_ADMIN) { _pause(); }
+    function unpause() external onlyRole(Roles.ROLE_ADMIN) { _unpause(); }
 }
 
 contract ArchitectureTest is Test {
     VaultStorageHarness vault;
 
     function setUp() public {
-        vault = new VaultStorageHarness(address(this), address(0xA11CE), address(0xFEE));
+        vault = new VaultStorageHarness(address(this), address(0xFEE));
     }
 
-    function testInitialConfig() public  view{
-        assertEq(vault.asset(), address(0xA11CE));
+    function testInitialConfig() public view {
         assertEq(vault.treasury(), address(0xFEE));
     }
 
